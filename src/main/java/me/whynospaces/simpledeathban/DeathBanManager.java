523b0105bfe2feb47b093cfeef98f152a878bd28
secapS <spaceslulzz@gmail.com>
2017-06-27 00:31:47 -1000

    Working on DeathBanManager

--- src/main/java/me/whynospaces/simpledeathban/DeathBanManager.java
+++ src/main/java/me/whynospaces/simpledeathban/DeathBanManager.java
@@ -1,25 +1,57 @@
 package me.whynospaces.simpledeathban;
 
 import lombok.Getter;
+import org.apache.commons.lang.Validate;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Collections;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class DeathBanManager {
 
     private JavaPlugin plugin;
     @Getter
-    private Map<UUID, Date> deathbans;
+    private Map<UUID, Long> deathbans = Collections.emptyMap();
 
     public DeathBanManager(JavaPlugin instance) {
         this.plugin = instance;
-        deathbans = Collections.emptyMap();
+        deathbans = new HashMap<>();
+        loadDeathBans();
     }
 
     public void loadDeathBans() {
-        
+        Validate.notEmpty(this.plugin.getConfig().getStringList("banned-players"), "There are no banned players.");
+        for(String bannedUUIDs : this.plugin.getConfig().getStringList("banned-players")) {
+            String[] split = bannedUUIDs.split(":");
+            UUID player = UUID.fromString(split[0]);
+            Long unbanTime = Long.parseLong(split[1]);
+            deathbans.put(player, unbanTime);
+        }
+    }
+
+    public boolean checkDeathBan(Player player) {
+        return System.currentTimeMillis() >= deathbans.get(player.getUniqueId());
+    }
+
+    public void addDeathBan(Player player) {
+        Validate.notNull(player, "Player cannot be null.");
+        Validate.isTrue(deathbans.containsKey(player.getUniqueId()), "Player is already death banned.");
+        deathbans.put(player.getUniqueId(), System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(this.plugin.getConfig().getLong("ban-time")));
+    }
+
+    public void removeDeathBan(Player player) {
+        Validate.notNull(player, "Player cannot be null.");
+        Validate.isTrue(deathbans.containsKey(player.getUniqueId()), "Player is not death banned.");
+        deathbans.remove(player.getUniqueId());
+        this.plugin.getConfig().set("banned-players." + player.getUniqueId().toString(), null);
+    }
+
+    public void deleteAllDeathbans(boolean cacheOnly) {
+        if(!cacheOnly) this.plugin.getConfig().set("banned-players", null);
+        deathbans = Collections.emptyMap();
     }
 }
--- src/main/java/me/whynospaces/simpledeathban/SimpleDeathBan.java
+++ src/main/java/me/whynospaces/simpledeathban/SimpleDeathBan.java
@@ -7,10 +7,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class SimpleDeathBan extends JavaPlugin {
 
     private CommandManager commandManager;
+    private DeathBanManager deathBanManager;
 
     @Override
     public void onEnable() {
         commandManager = ACF.createManager(this);
+        deathBanManager = new DeathBanManager(this);
     }
 
     @Override
